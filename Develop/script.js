9ed54f263f71fca153b09abb966fc19505886d31
Olloyd321 <olloyd321@gmail.com>
2023-01-13 14:35:40 -0500

    created password options

--- Develop/script.js
+++ Develop/script.js
@@ -5,15 +5,38 @@ var symbols = ['!','@','#']
 var upperCase = ['A','B','C','D']
 var lowerCase = ['a','b','c','d']
 
-function getRandom(){
-  
+function getRandom(arr){
+  var randomIndex = Math.floor(Math.random()*arr.length)
+  var randomArray = arr[randomIndex]  
 }
 
 function passwordOption(){
+  var length = parseInt(
+    prompt('How long do you want your password to be?')
+  )
+  if (Number.isNaN(length)){
+    alert('Must be a integer')
+    return null
+  }
+  if(length<8||length>128){
+    alert('Must be between 8-128 characters')
+    return null
+  }
+  var wantSpecial = confirm('click OK to confirm special characters.')
+  var wantNumber = confirm('click OK to confirm number.')
+  var wantUpper = confirm('click OK to confirm UpperCase.')
+  var wantLower = confirm('click OK to confirm LowerCase')
 
+  var options = {
+    length:length,
+    wantSpecial:wantSpecial,
+    wantLower:wantLower,
+    wantUpper:wantUpper,
+  }
+  return options
 }
 
-function genreatePassword(){
+function generatePassword(){
 
 }
 
